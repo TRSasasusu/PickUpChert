ebd4d4db392f5bcacd122edb9e57319ce5d81492
Kaito Kishi <17728957+TRSasasusu@users.noreply.github.com>
2024-01-21 23:11:29 +0900

    chert can speak about sunless city in that sector

--- PickUpChert/ChertPickUpConversation.cs
+++ PickUpChert/ChertPickUpConversation.cs
@@ -9,6 +9,10 @@ namespace PickUpChert {
     public class ChertPickUpConversation : MonoBehaviour {
         public static ChertPickUpConversation Instance;
 
+        static readonly Dictionary<string, string> SECTOR_DIALOGUE_DICT = new Dictionary<string, string>() {
+            {"Sector_City", "sunless_city"},
+        };
+
         enum InitialPickUpState {
             BEFORE_PICKUP,
             AFTER_PICKUP,
@@ -17,10 +21,21 @@ namespace PickUpChert {
         InitialPickUpState _initialPickUpState = InitialPickUpState.BEFORE_PICKUP;
 
         static TextAsset _initialPickUpXML;
+        static Dictionary<string, TextAsset> _sectorXMLDict;
+
+        Sector _currentSector;
 
         public static void Initialize() {
-            var path = PickUpChert.Instance.ModHelper.Manifest.ModFolderPath + $"assets/dialogues/initial_pick_up.xml";
-            _initialPickUpXML = new TextAsset(PickUpChert.ReadAndRemoveByteOrderMarkFromPath(path));
+            {
+                var path = PickUpChert.Instance.ModHelper.Manifest.ModFolderPath + $"assets/dialogues/initial_pick_up.xml";
+                _initialPickUpXML = new TextAsset(PickUpChert.ReadAndRemoveByteOrderMarkFromPath(path));
+            }
+
+            _sectorXMLDict = new Dictionary<string, TextAsset>();
+            foreach(var (sectorName, dialogueFileName) in SECTOR_DIALOGUE_DICT) {
+                var path = PickUpChert.Instance.ModHelper.Manifest.ModFolderPath + $"assets/dialogues/{dialogueFileName}.xml";
+                _sectorXMLDict.Add(sectorName, new TextAsset(PickUpChert.ReadAndRemoveByteOrderMarkFromPath(path)));
+            }
         }
 
         public bool OnStartConversation() {
@@ -28,6 +43,24 @@ namespace PickUpChert {
                 BringChert.Instance.ChertDialogueTree.SetTextXml(_initialPickUpXML);
                 _initialPickUpState = InitialPickUpState.END_INITIAL_PICKUP;
                 return false;
+            }
+
+            if(!_currentSector) {
+                return true;
+            }
+
+            var sector = _currentSector;
+            for(int i = 0; i < 1000; ++i) {
+                foreach(var (sectorName, xml) in _sectorXMLDict) {
+                    if(sector.name == sectorName) {
+                        BringChert.Instance.ChertDialogueTree.SetTextXml(xml);
+                        return false;
+                    }
+                }
+                sector = sector._parentSector;
+                if(!sector) {
+                    break;
+                }
             }
             return true;
         }
@@ -44,12 +77,9 @@ namespace PickUpChert {
                 if(ChertItem.Instance.Brought) {
                     _initialPickUpState = InitialPickUpState.AFTER_PICKUP;
                 }
-                else {
-                    return;
-                }
             }
 
-            var sector = BringChert.Instance.SectorDetector.GetLastEnteredSector();
+            _currentSector = BringChert.Instance.SectorDetector.GetLastEnteredSector();
         }
     }
 }
