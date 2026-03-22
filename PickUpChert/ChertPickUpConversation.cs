5f3d1f4a816524fbcea367b9fa138b97426ac82e
Kaito Kishi <17728957+TRSasasusu@users.noreply.github.com>
2026-03-23 00:38:14 +0900

    introduce unirx and trigger for changing conversation works

--- PickUpChert/ChertPickUpConversation.cs
+++ PickUpChert/ChertPickUpConversation.cs
@@ -5,6 +5,11 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using UnityEngine;
+using UniRx;
+using UniRx.Triggers;
+using NewHorizons.Utility;
+using System.IO;
+using IEnumerator = System.Collections.IEnumerator;
 
 namespace PickUpChert {
     public class ChertPickUpConversation : MonoBehaviour {
@@ -19,8 +24,10 @@ namespace PickUpChert {
 
         static TextAsset _initialPickUpXML;
         static Dictionary<string, TextAsset> _sectorXMLDict;
+        static Dictionary<string, TextAsset> _triggerXMLDict;
 
         Sector _currentSector;
+        TextAsset _triggerXML;
 
         public static void Initialize() {
             {
@@ -45,12 +52,26 @@ namespace PickUpChert {
                     }
                 }
             }
+
+            _triggerXMLDict = new Dictionary<string, TextAsset>();
+            foreach(var dialogueFilePath in System.IO.Directory.EnumerateFiles(PickUpChert.Instance.ModHelper.Manifest.ModFolderPath + "assets/dialogues/outerwilds/trigger", "*.xml", System.IO.SearchOption.AllDirectories)) {
+                PickUpChert.Log(dialogueFilePath);
+                var dirName = new DirectoryInfo(dialogueFilePath).Parent.Name;
+                var fileName = System.IO.Path.GetFileNameWithoutExtension(dialogueFilePath);
+                var triggerName = $"PUCTriggers{dirName}/{fileName}";
+                _triggerXMLDict[triggerName] = new TextAsset(PickUpChert.ReadAndRemoveByteOrderMarkFromPath(dialogueFilePath));
+            }
         }
 
         public bool OnStartConversation() {
             if(_initialPickUpState == InitialPickUpState.AFTER_PICKUP) {
                 BringChert.Instance.ChertDialogueTree.SetTextXml(_initialPickUpXML);
                 _initialPickUpState = InitialPickUpState.END_INITIAL_PICKUP;
+                return false;
+            }
+
+            if(_triggerXML != null) {
+                BringChert.Instance.ChertDialogueTree.SetTextXml(_triggerXML);
                 return false;
             }
 
@@ -76,6 +97,37 @@ namespace PickUpChert {
 
         void Awake() {
             Instance = this;
+        }
+
+        IEnumerator Start() {
+            yield return null;
+            PickUpChert.Log(GameObject.Find("PUCTriggersTH/Campsite").name);
+            foreach (var (triggerName, xml) in _triggerXMLDict) {
+                while (true) {
+                    var trigger = GameObject.Find(triggerName);
+                    if (trigger) {
+                        PickUpChert.Log("found trigger: " + triggerName);
+                        trigger.OnTriggerEnterAsObservable().Subscribe(other => {
+                            PickUpChert.Log("entered trigger: " + triggerName);
+                            if (other.gameObject == Locator._playerBody.gameObject) {
+                                PickUpChert.Log("player entered trigger: " + triggerName);
+                                _triggerXML = xml;
+                            }
+                        }).AddTo(trigger);
+                        trigger.OnTriggerExitAsObservable().Subscribe(other => {
+                            PickUpChert.Log("exited trigger: " + triggerName);
+                            if (other.gameObject == Locator._playerBody.gameObject) {
+                                PickUpChert.Log("player exited trigger: " + triggerName);
+                                if(_triggerXML == xml) {
+                                    _triggerXML = null;
+                                }
+                            }
+                        }).AddTo(trigger);
+                        break;
+                    }
+                    yield return null;
+                }
+            }
         }
 
         void Update() {
